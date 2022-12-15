2eafa184eaa9118ff9b110f188ed932da8f10524
salsabilaasyifa <salsaasyifass@gmail.com>
2022-12-15 20:14:40 +0700

    fix tst

--- app.py
+++ app.py
@@ -13,23 +13,25 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 import pymysql
 
 
-def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
+from sqlalchemy import create_engine
 
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username="root",
-            password="salsabilaas",
-            database="tubes_tst",
-            query={"unix_socket": "/cloudsql/tubes-tst-371605:asia-southeast2:tubes-tst"},
-        ),
-        # ...
-    )
-    return pool
-# engine = sqlalchemy.create_engine('mysql+pymysql://root:salsabilaas@34.101.123.23:3306/tubes_tst')
-engine = connect_unix_socket()
+db_user = "root" # ex : root
+db_password = "salsabilaas"
+db_host = "34.101.123.23" # ex : localhost
+db_port = 3306 # ex :3306
+db_database = "tubes_tst" #ex : db_afkar
+db_sslmode = True
+
+# ini klo pake mysql
+db_engine = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
+
+engine = create_engine(db_engine)
+
+try:
+    conn = engine.connect()
+except:
+    raise
+
 Session = scoped_session(sessionmaker(bind=engine))
 
 def hash_password(password):
@@ -94,6 +96,34 @@ def pd(user):
         json_data.append(dict(zip(row_headers,result)))
     cur.close()
     return json.dumps({"penerimaan dana": json_data})
+
+@app.route("/dana-infrastruktur")
+@token
+def pd(user):
+    cur = conn
+    rows = cur.execute(
+    "SELECT nama_sekolah, (Penerimaan_Dana_TW_1_Rp + Penerimaan_Dana_TW_2_Rp + Penerimaan_Dana_TW_3_Rp + Penerimaan_Dana_TW_4_Rp) * 0.3 AS total FROM penerimaandana")
+    row_headers=[x[0] for x in cur.description]
+    rows = cur.all()
+    json_data=[]
+    for result in rows:
+        json_data.append(dict(zip(row_headers,result)))
+    cur.close()
+    return json.dumps({"dana infrastruktur": json_data})
+
+@app.route("/dana-infrastruktur")
+@token
+def pd(user):
+    cur = conn
+    rows = cur.execute(
+    "SELECT nama_sekolah, (Penerimaan_Dana_TW_1_Rp + Penerimaan_Dana_TW_2_Rp + Penerimaan_Dana_TW_3_Rp + Penerimaan_Dana_TW_4_Rp) * 0.3 AS total FROM penerimaandana")
+    row_headers=[x[0] for x in cur.description]
+    rows = cur.all()
+    json_data=[]
+    for result in rows:
+        json_data.append(dict(zip(row_headers,result)))
+    cur.close()
+    return json.dumps({"dana infrastruktur": json_data})
 
 @app.route("/delete-pd", methods=["DELETE"])
 @token
@@ -272,24 +302,6 @@ def checkUserAvailable(cur, body):
         json_data=(dict(zip(row_headers,result)))
     
     return json_data
-
-# def getDanaSetahun(cur, id):
-#     rows = cur.execute(
-#         f"SELECT Penerimaan_Dana_TW_1_Rp + Penerimaan_Dana_TW_2_Rp + Penerimaan_Dana_TW_3_Rp + Penerimaan_Dana_TW_4_Rp AS total FROM penerimaandana WHERE id={id}"
-#     )
-#     row_headers=[x[0] for x in cur.description]
-#     rows = cur.all()
-#     json_data= None
-#     for result in rows:
-#         json_data=(dict(zip(row_headers,result)))
-    
-#     return json_data
-
-# def getDanaInfrastruktur(id):
-#     cur = conn
-#     total = getDanaSetahun(cur,id)
-#     result = total * 0.3
-#     return result
 
 # def getJumlahSiswa(id):
 #     cur = conn
